949474a96af6e25b1804e1b645ce39bc257d3ce5
Matthew Todd <matthew.todd@gmail.com>
2009-07-25 11:20:44 +0300

    Whack tmp before running features.
    Makes it easier to poke around post-mortem, and saves on disk space, too.

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,9 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', '..', 'lib'))
+require 'pathname'
+
+project_root = Pathname.new(__FILE__).dirname.parent.parent
+$LOAD_PATH.unshift(project_root.join('lib'))
 require 'captain'
+
+project_root.join('tmp').rmtree
 
 Before { @app = Captain::Application.new }
