e5258a95c0b661238bb3a3d8291360473eb8bb8f
Matthew Todd <matthew.todd@gmail.com>
2010-02-16 17:27:02 +0300

    Configure autotest.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source :gemcutter
 
 gem 'captain', :path => File.expand_path('..', __FILE__)
 
+gem 'autotest'
 gem 'cucumber'
 gem 'redgreen'
 gem 'shoe'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ require 'captain'
 require 'test/unit'
 require 'shoulda/test_unit'
 
-if $stdout.tty?
+if $stdout.tty? || ENV.has_key?('AUTOTEST')
   require 'redgreen'
 end
 
