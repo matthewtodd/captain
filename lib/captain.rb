dc0379b09679698061c0d31abd6a4a46f017a1a9
Matthew Todd <matthew.todd@gmail.com>
2010-02-16 13:55:48 +0300

    Prefer autoload to explicit requires.

--- lib/captain.rb
+++ lib/captain.rb
@@ -1,10 +1,11 @@
-require 'captain/application'
-require 'captain/image'
-require 'captain/package'
-require 'captain/package_list'
-require 'captain/release'
-require 'captain/remote'
-require 'captain/resource'
+require 'captain/version'
 
 module Captain
+  autoload :Application, 'captain/application'
+  autoload :Image,       'captain/image'
+  autoload :Package,     'captain/package'
+  autoload :PackageList, 'captain/package_list'
+  autoload :Release,     'captain/release'
+  autoload :Remote,      'captain/remote'
+  autoload :Resource,    'captain/resource'
 end
